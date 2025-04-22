4743983eb6392fb587f4cffd50ddfada6434c010
AdrianFalasca <138259951+AdrianFalasca@users.noreply.github.com>
2025-04-21 23:11:52 -0300

    Update Firebase.js

--- src/Config/Firebase.js
+++ src/Config/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAVeMKEk2zQLd6lpSOHcfQa9OduotJiBJs",
-    authDomain: "myapp-45598.firebaseapp.com",
-    projectId: "myapp-45598",
-    storageBucket: "myapp-45598.appspot.com",
-    messagingSenderId: "131279734394",
-    appId: "1:131279734394:web:00abb154c0f6766944ddc3",
-    measurementId: "G-F2Y61NYYYW"
+    //vaciado
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
